0c8b371df6195b43e98fa039e3529c69a7a2aab3
Ben Holland <benjamin.holland1@gmail.com>
2017-09-16 15:09:27 -0600

    Added CMake Files Added Camera Functionality for Logitech C270 webcam

---
+++ CMakeFiles/SwarmHost.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SwarmHost.dir/main.cpp.o"
+  "SwarmHost.pdb"
+  "SwarmHost"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SwarmHost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
